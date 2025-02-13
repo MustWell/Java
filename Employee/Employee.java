bc8a312655f9136d9bb072a8f9e6b2b6cfe5462b
Simoneshko Maksim <128395581+MustWell@users.noreply.github.com>
2025-02-13 13:45:52 +0300

    Add files via upload

---
+++ Employee/Employee.java
@@ -0,0 +1,34 @@
+
+package Employee;
+
+public class Employee {
+
+
+    private String empName;
+    private final String empCompany;
+    private final int age;
+    private final int zp;
+
+    public Employee() {
+        this.empName = ArrNameAndComp.arrName();
+        this.empCompany = ArrNameAndComp.arrCompany();
+        this.age = Age.getRandomAge();
+        this.zp = Salary.getRandomZp();
+    }
+
+    public Employee(String empName, String empCompany, int age, int zp) {
+        this.empCompany = empCompany;
+        this.age = age;
+        this.zp = zp;
+    }
+
+    @Override
+    public String toString() {
+        return "\nEmployee\n________" +
+                "\nName = " + empName +
+                "\nCompany = " + empCompany  +
+                "\nAge = " + age +
+                "\nSalary = " + zp + "$";
+    }
+
+}
