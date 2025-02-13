635787e8f6d2b3f69c33c2064ffb4978d1f82d48
Simoneshko Maksim <128395581+MustWell@users.noreply.github.com>
2025-02-13 13:44:54 +0300

    Add files via upload

---
+++ Array100EL/Main.java
@@ -0,0 +1,11 @@
+package Array100EL;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Array100EL.Arr array = new Array100EL.Arr();
+
+        array.delete(3);
+        array.Conclusion();
+    }
+}
